e0b30656f215d37adca860a3aac236221f76d7e7
DanieleAntonaci <danieleanto96@gmail.com>
2022-12-07 15:40:47 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script>
+    export default{
+        data(){
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+
+        <img src="../assets/vue.svg" class="logo" >
+    </div>
+</template>
+
+<style scoped>
+    .logo {
+    height: 6em;
+    padding: 1.5em;
+    }
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+    export default{
+        data(){
+            return{
+                name:'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+</template>
+
+<style scoped>
+
+</style>
